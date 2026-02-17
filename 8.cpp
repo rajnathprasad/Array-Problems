8cc436823c9e02a0293144c663d7aa72246c5f70
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-18 00:49:25 +0530

    8

--- 8.cpp
+++ 8.cpp
@@ -1,1 +1,20 @@
 // 8. Find Pair with Given Sum: Find a pair of elements that adds up to a target sum.
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_set<int> s;
+        for(int i=0;i<arr.size();i++){
+            int complement = target-arr[i];
+            if(s.find(complement)!=s.end()){
+                return true;
+            }
+            s.insert(arr[i]);
+        }
+        return false;
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
