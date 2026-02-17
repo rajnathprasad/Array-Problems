7f6452f36cae63b3441d832e7327779723d55b49
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-17 18:51:07 +0530

    1

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+// 1. Reverse an Array
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int low = 0;
+        int high = arr.size()-1;
+        while(low<high){
+            swap(arr[low],arr[high]);
+            low++;
+            high--;
+        }
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
