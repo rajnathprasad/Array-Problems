2e5a1fb6168fea2eeba48e5e151717893ba4cdd3
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-17 23:59:13 +0530

    7

--- 6.cpp
+++ 6.cpp
@@ -1,1 +1,19 @@
 // 6. Check if Array is Sorted
+
+class Solution {
+  public:
+    bool isSorted(vector<int>& arr) {
+        if(arr.size()<=1){
+            return true;
+        }
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
--- 7.cpp
+++ 7.cpp
@@ -1,1 +1,15 @@
 // 7. Rotate Array by k Positions: Rotate the array to the right by k positions.
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+}
+
+
+// TC : O(N)
+// SC : O(1)
