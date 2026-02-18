7808af5b16b9053f6f0aed88fcb2bf83be7179c7
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-18 13:08:07 +0530

    9

--- 9.cpp
+++ 9.cpp
@@ -1,1 +1,22 @@
 // 9. Remove Duplicates from Array: Remove duplicates from the array while maintaining order.
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1){
+            return n;
+        }
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+// TC : O(N)
+// SC : O(1)
