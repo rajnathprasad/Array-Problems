a96022349323cf378b4561c2c29a384669b4a4b3
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-22 10:57:48 +0530

    26

---
+++ 26.cpp
@@ -0,0 +1,22 @@
+// 26. Find Peak Element: A peak element is greater than its neighbors. Find one such element.
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        for(int i=1;i<n-1;i++){
+            if(nums[i-1]<nums[i] && nums[i+1]<nums[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+// TC : O(n)
+// SC : O(1)
