75c89aa982ab112598dab69dd7496029f7352bb7
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-25 13:23:09 +0530

    27

---
+++ 27.cpp
@@ -0,0 +1,22 @@
+// 27. Find the First Missing Positive: Find the smallest positive integer missing in the array.
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1] !=nums[i]){
+                swap(nums[nums[i]-1],nums[i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
+
+// TC : O(n)
+// SC : O(1)
